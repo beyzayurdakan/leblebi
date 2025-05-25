8e8c19a24995fe013c32302cc7418fc26fec3e7c
beyzayurdakan <beyzayurdakan@gmail.com>
2025-05-26 01:55:29 +0300

    deneme3

--- backend/config.py
+++ backend/config.py
@@ -5,19 +5,17 @@ import json
 
 load_dotenv()
 
+# Google kimlik bilgilerini geçici dosyaya yaz
+if "gcp_service_account" in st.secrets:
+    with open("gcp_key.json", "w") as f:
+        json.dump(dict(st.secrets["gcp_service_account"]), f)
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "gcp_key.json"
 
 
 os.environ["PINECONE_API_KEY"] = st.secrets["PINECONE_API_KEY"]
 os.environ["PINECONE_ENV"] = st.secrets["PINECONE_ENV"]
 os.environ["VERTEX_PROJECT_ID"] = st.secrets["VERTEX_PROJECT_ID"]
 os.environ["VERTEX_REGION"] = st.secrets["VERTEX_REGION"]
-
-
-# Google kimlik doğrulamasını ayarla
-if "gcp_service_account" in st.secrets:
-    with open("gcp_key.json", "w") as f:
-        json.dump(st.secrets["gcp_service_account"], f)
-    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "gcp_key.json"
 
 
 PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
