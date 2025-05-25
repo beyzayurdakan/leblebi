58d7c4d692bd178eb37624a7a1b70845ae338c08
beyzayurdakan <beyzayurdakan@gmail.com>
2025-05-26 01:19:51 +0300

    GCP kimlik doğrulama ve secrets entegrasyonu eklendi

--- backend/config.py
+++ backend/config.py
@@ -1,7 +1,24 @@
 import os
 from dotenv import load_dotenv
+import streamlit as st
+import json
 
 load_dotenv()
+
+
+
+os.environ["PINECONE_API_KEY"] = st.secrets["PINECONE_API_KEY"]
+os.environ["PINECONE_ENV"] = st.secrets["PINECONE_ENV"]
+os.environ["VERTEX_PROJECT_ID"] = st.secrets["VERTEX_PROJECT_ID"]
+os.environ["VERTEX_REGION"] = st.secrets["VERTEX_REGION"]
+
+
+# Google kimlik doğrulamasını ayarla
+if "gcp_service_account" in st.secrets:
+    with open("gcp_key.json", "w") as f:
+        json.dump(st.secrets["gcp_service_account"], f)
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "gcp_key.json"
+
 
 PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
 PINECONE_ENV = os.getenv("PINECONE_ENV", "us-east-1")
